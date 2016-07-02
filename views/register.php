a7bd4b70604e68f931f3b510aa16be7d89043d02
Humphrey Mugambi <mugagambi@gmail.com>
2016-07-02 04:54:36 +0300

    Added the Registration form

--- views/register.php
+++ views/register.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Login - Hogwarts High School</title>
+    <title>Register- Hogwarts High School</title>
 
     <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
     <link href="../assests/css/bootstrap.min.css" rel="stylesheet">
@@ -22,15 +22,34 @@
     <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
     <!--Custom css -->
-    <link rel="stylesheet" href="../assest/css/style.css">
-    <link rel="stylesheet" href="../assests/css/login.css">
-    <!-- HTML5 Shim and Respond.js IE8 sup
-    port of HTML5 elements and media queries -->
+      <link href="../assests/css/style.css" rel="stylesheet">
+      
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <style type="text/css">
+  body{
+    background-color:#AfA5A5 ;
+}
+.centered-form{
+	margin-top: 60px;
+}
+
+.centered-form .panel{
+	background: rgba(255, 255, 255, 0.8);
+	box-shadow: rgba(0, 0, 0, 0.3) 20px 20px 20px;
+}
+	.more {
+		padding-top: 150px;
+	}
+
+
+    </style>
+    
 
 </head>
 
@@ -47,7 +66,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#page-top">Hogwarts High</a>
+                <a class="navbar-brand page-scroll" href="#page-top">Hogwarts High</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
@@ -76,26 +95,59 @@
             <!-- /.navbar-collapse -->
         </div>
         <!-- /.container-fluid -->
-    </nav> 
-  </head>
+    </nav>
+   <div class="container more">
+        <div class="row centered-form">
+        <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+        	<div class="panel panel-default">
+        		<div class="panel-heading">
+			    		<h3 class="panel-title">STUDENT REGISTIRATION</h3>
+			 			</div>
+			 			<div class="panel-body">
+			    		<form role="form">
+			    			<div class="row">
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			                <input type="text" name="first_name" id="first_name" class="form-control input-sm" placeholder="First Name">
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="text" name="last_name" id="last_name" class="form-control input-sm" placeholder="Last Name">
+			    					</div>
+			    				</div>
+			    			</div>
 
-  <body>
+			    			<div class="form-group">
+			    				<input type="email" name="email" id="email" class="form-control input-sm" placeholder="Email Address">
+			    			</div>
+			    			<div class="form-group">
+			    				<input type="text" name="reg_no" id="reg_no" class="form-control input-sm" placeholder="Registration Number">
+			    			</div>
 
-      <div class="wrapper">
-    <form class="form-signin">       
-      <h2 class="form-signin-heading">Student Login</h2>
-      <input type="text" class="form-control" name="username" placeholder="Registration Number" required="" autofocus="" />
-      <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
-      <label class="checkbox">
-        <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
-      </label>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>   
-    </form>
-  </div>
-    
-    
-    
-    
-    
-  </body>
-</html>
+
+			    			<div class="row">
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="password" name="password" id="password" class="form-control input-sm" placeholder="Password">
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-sm" placeholder="Confirm Password">
+			    					</div>
+			    				</div>
+			    			</div>
+			    			
+			    			<input type="submit" value="Register" class="btn btn-info btn-block">
+			    		
+			    		</form>
+			    	</div>
+	    		</div>
+    		</div>
+    	</div>
+    </div>
+	<script src="../assests/js/jquery-1.12.3.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../assests/js/bootstrap.min.js"></script>
